b4789c6477e5327d3485a9f50f0374271f66e01a
tatiana-2 <tatianaguzmansaavedra@gmail.com>
2022-11-28 22:31:15 -0500

    Ejercicios javaScrip
    aqui se realizaron ejercicios basicos imprimiendo en pantalla

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert("Hola Mundo esttoy aprendindo javaScrip");
+alert("Dos");
+alert("Tres")
+alert("Cuatro");
+alert("Cinco");
+//
+console.log(1234567);
+console.log("David Uruña");
+console.log(true);
+console.log(false);
+
+document.write(Hello );
